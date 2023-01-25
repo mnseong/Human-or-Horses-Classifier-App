71b4df9f8e510d836a93cf0124834fb323827797
mnseong <mskweon@icloud.com>
2023-01-26 03:21:32 +0900

    Initial Commit

---
+++ HumanOrHorsesClassifierApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HumanOrHorsesClassifierApp
+//
+//  Created by 권민성 on 2023/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
